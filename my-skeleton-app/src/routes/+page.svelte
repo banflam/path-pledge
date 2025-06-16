9e937706378df710a7ce5f5ab0ebeadfd881fc6b
banflam <rishiagrawal2002@gmail.com>
2025-06-16 18:40:52 -0400

    added the path pledge to the webpage

--- my-skeleton-app/src/routes/+page.svelte
+++ my-skeleton-app/src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>The PATH Pledge</h1>
+<p>"Join OFTEN UPDATED NUMBER people who have taken the PATH pledge for a better ride"</p>
+
+<p>I am a PATH rider who wants a better experience. I pledge to do the following:</p>
+
+<ol>
+    <li>Stand to the <em>side</em> of the door to <em>let all people off</em> before boarding</li>
+    <li><em>Move in</em> to the center of the train to make space for others to board</li>
+</ol>
+
